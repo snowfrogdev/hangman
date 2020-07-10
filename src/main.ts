387d8a48c59c35f4f849993ca66ec72c052319da
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2020-07-10 01:42:32 -0400

    Display masked secret word after each guess and display correctly guessed letters in the proper position in the word

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { Hangman } from "./hangman";
 const game = new Hangman('Vegemite', 6);
 
 const secretWord = document.getElementById('secretWord') as HTMLHeadingElement;
+const guess = document.getElementById('guess') as HTMLInputElement;
+const button = document.querySelector('button') as HTMLButtonElement;
+
+button.addEventListener('click', (e) => {
+  game.guess(guess.value);
+  secretWord.textContent = game.maskedSecretWord;
+  guess.value = '';
+
+})
 
 secretWord.textContent = game.maskedSecretWord;
 
