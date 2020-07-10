bf3dafb07300a8d47639292e2a7269a39e57efd1
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2020-07-10 02:18:28 -0400

    Display message if game is won or lost

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,21 @@ const guess = document.getElementById('guess') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const incorrectGuesses = document.getElementById('incorrectGuesses') as HTMLParagraphElement;
 const remainingGuesses = document.getElementById('remainingGuesses') as HTMLHeadingElement;
+const messageCenter = document.getElementById('messageCenter') as HTMLHeadingElement;
+
+secretWord.textContent = game.maskedSecretWord;
+updateRemainingGuesses();
+messageCenter.textContent = '🎶🎵🎶 Welcome to jungle, we got fun and games 🎶🎵🎶';
 
 button.addEventListener('click', (e) => {
-  game.guess(guess.value);
+  const result = game.guess(guess.value);
+  messageCenter.textContent = result;
   secretWord.textContent = game.maskedSecretWord;
   incorrectGuesses.textContent = [...game.incorrectGuesses].join(', ');
   updateRemainingGuesses();
   guess.value = '';
 });
 
-updateRemainingGuesses();
-
-secretWord.textContent = game.maskedSecretWord;
-
 function updateRemainingGuesses() {
-  remainingGuesses.textContent = `Remaining incorrect guesses before total destruction: ${game.remainingGuesses}`;
+  remainingGuesses.textContent = `You are allowed ${game.remainingGuesses} incorrect guesses`;
 }
